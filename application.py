3afe6af980ad3a0f7628e0d5145c2f5227f0bc3f
AlexandraHuch <AlexandraHuch@users.noreply.github.com>
2020-05-08 17:32:20 +0200

    py

--- application.py
+++ application.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-    return render_template("test.html")
+def cookingblog():
+    return render_template("cookingblog.html")
 
 @app.route("/ramen")
 def ramen():
